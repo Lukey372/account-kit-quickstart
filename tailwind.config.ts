f9c8dc95ee8103bc6e4a37892c457100a2712499
Luke <100325058+Lukey372@users.noreply.github.com>
2025-04-10 17:09:05 +1000

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import { createColorSet, withAccountKitUi } from "@account-kit/react/tailwind";
-
-export default withAccountKitUi({
+export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,9 +8,5 @@ export default withAccountKitUi({
   theme: {
     extend: {},
   },
-  colors: {
-    "btn-primary": createColorSet("#E82594", "#FF66CC"),
-    "fg-accent-brand": createColorSet("#E82594", "#FF66CC"),
-  },
   plugins: [],
-});
+}
